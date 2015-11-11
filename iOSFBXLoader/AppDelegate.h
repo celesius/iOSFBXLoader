aa8aaac0d8a8131f6c465bdd0db645c83c543c3a
BoJiang <188166614@qq.com>
2015-11-11 10:27:58 +0800

    Initial Commit

---
+++ iOSFBXLoader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSFBXLoader
+//
+//  Created by vk on 15/11/11.
+//  Copyright © 2015年 clover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSFBXLoader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSFBXLoader
+//
+//  Created by vk on 15/11/11.
+//  Copyright © 2015年 clover. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
